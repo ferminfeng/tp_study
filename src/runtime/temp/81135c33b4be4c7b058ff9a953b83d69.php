a6f4a492ddd8b4f88def0f0dacde8fe8755027d6
fyflzjz <fermin@fuyifengdeMBP.lan>
2016-10-21 21:35:56 +0800

    删除runtime

--- src/runtime/temp/81135c33b4be4c7b058ff9a953b83d69.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:83:"E:\fyflzjz_github\tp_study\src\public/../application/index\view\index\index_db.html";i:1476933996;}*/ ?>
-<html>
-<head>
-<title></title>
-</head>
-<body>
-<?php echo $result['id']; ?>--<?php echo $result['data']; ?>
-</body>
-</html>
